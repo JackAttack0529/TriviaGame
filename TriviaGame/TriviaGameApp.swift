8ed433c28ebb593dacabdb1bf188c769d4171e50
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-03-13 01:32:52 -0400

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Matthew RIley on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
